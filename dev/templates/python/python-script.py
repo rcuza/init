7d5b696792e32e679ceabd54851dd8413f46f91e
Raul Cuza <raulcuza@gmail.com>
2015-09-22 23:38:42 -0400

    Add from __future__ features

--- dev/templates/python/python-script.py
+++ dev/templates/python/python-script.py
@@ -6,6 +6,7 @@ ${TM_NEW_FILE_BASENAME}.py
 Created by ${TM_FULLNAME} on ${TM_DATE}.
 Copyright (c) ${TM_YEAR} ${TM_ORGANIZATION_NAME}. All rights reserved.
 """
+from __future__ import absolute_import, division, print_function
 
 import sys
 import os

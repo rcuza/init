59c3a07daaab3444ecca93e6b32716fcfd473f18
Raúl Cuza <raulcuza@gmail.com>
2017-09-21 22:40:49 -0400

    update the makemacmine script

--- OSX/bin/makemacmine.bash
+++ OSX/bin/makemacmine.bash
@@ -12,27 +12,87 @@ else
   exit
 fi
 
-brew cask install calibre
-brew cask install encryptr
-brew cask install google-chrome
-brew cask install kindle
-brew cask install libreoffice  # productivity
-brew cask install nightingale  # music player
-brew cask install menumeters   # ref below
-brew cask install send-to-kindle
-brew cask install textmate
-brew cask install vlc
+brew update
 
-# UNIX Tools
-brew install macvim
+echo "### brew some unix commands"
+brew install \
+    awscli \
+    consul \
+    git \
+    go \
+    graphviz \
+    python          \
+    python3         \
+    terraform \
+    vagrant-completion \
+    vault
 
-
-# Some PIP
-
-# install AWS Command Line Tools
-sudo pip install awscli
-
+echo "### brew some mac apps (cask)"
+brew cask install \
+    calibre         \
+    colloquy \
+    encryptr        \
+    google-chrome   \
+    iterm2 \
+    kindle          \
+    libreoffice     \
+    mactex          \
+    macvim          \
+    nightingale     \
+    menumeters      \
+    slack \
+    textmate        \
+    vagrant \
+    virtualbox \
+    vlc \
+    yujitach-menumeters
 
 # Reference
 # MenuMeters
 # http://www.ragingmenace.com/software/menumeters
+
+####
+# Install Janus
+# https://raw.githubusercontent.com/carlhuda/janus/master/bootstrap.sh (4a7a9f2)
+####
+function die()
+{
+    echo "${@}"
+    exit 1
+}
+
+function backup_previous_install()
+{
+  # Add .old to any existing Vim file in the home directory
+  for filepath in "${HOME}/.vim" "${HOME}/.vimrc" "${HOME}/.gvimrc"; do
+    if [ -e $filepath ]; then
+      mv "${filepath}" "${filepath}.old" || die "Could not move ${filepath} to ${filepath}.old"
+      echo "${filepath} has been renamed to ${filepath}.old"
+    fi
+  done
+}
+
+function clone_janus()
+{
+  # Clone Janus into .vim
+  git clone --recursive https://github.com/carlhuda/janus.git "${HOME}/.vim" \
+    || die "Could not clone the repository to ${HOME}/.vim"
+}
+
+function run_rake()
+{
+  # Run rake inside .vim
+  pushd "${HOME}/.vim" || die "Could not go into the ${HOME}/.vim"
+  rake || die "Rake failed."
+  popd
+}
+
+function install_janus()
+{
+  if [ ! -e "${HOME}/.vim/janus" -o "$1" == "--force" ]; then
+    backup_previous_install
+    clone_janus
+  fi
+  run_rake
+}
+install_janus
